98a34bd5e1ab95198169cfb1d2f329690a9c25aa
raiyan-ahmed <rfraiyan123@gmail.com>
2022-11-04 02:33:57 +0600

    initial commit

---
+++ include/footer.php
@@ -0,0 +1,7 @@
+<footer>
+	<div class="container">
+		Copyright <span class="glyphicon glyphicon-copyright-mark"></span> RA Stores.
+		All Rights Reserved | Contact Us: +880 1791271560
+	</div>
+</footer>
+
